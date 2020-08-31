704b18d2d70a6c02beac0eeec1a431b2549ce401
Hamza Safdar <42969550+hamzasafdar01@users.noreply.github.com>
2020-08-31 23:00:57 +0500

    pug practice

---
+++ 31-08-2020/app.js
@@ -0,0 +1,26 @@
+const path= require('path');
+
+const express = require('express');
+const bodyParser=  require('body-parser');
+
+const productData= require('./routes/admin');
+const shop= require('./routes/shop');
+
+const app= express();
+app.set('view engine','pug');
+app.set('views','views');
+
+
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/admin', productData.routes);
+app.use(shop);
+
+app.use((req, res, next)=>{
+    res.status(404).render('404');
+    // res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    // res.status(404).send('<h1>Page Not Found</h1>')
+});
+
+app.listen(3000);
